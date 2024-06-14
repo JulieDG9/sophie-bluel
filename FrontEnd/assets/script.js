86e8123257654685a2e1d4a30a386d1fdc19396b
prosg <learner.in.dev@gmail.com>
2024-06-14 10:11:54 +0200

    MaJ galerie JS

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,25 +1,28 @@
+const apiUrl = "http://localhost:5678/api";
+
 const gallery = document.querySelector(".gallery");
 
-// const url = "http://localhost:5678/api/works";
-// fetch(url)
-fetch("http://localhost:5678/api/works")
+function createImg(data) {
+  data.forEach((index) => {
+    const figureElement = document.createElement("figure");
+    const imgElement = document.createElement("img");
+    const captionElement = document.createElement("figcaption");
+
+    imgElement.src = index.imageUrl;
+    imgElement.alt = index.title;
+    captionElement.textContent = index.title;
+
+    figureElement.dataset.categoryId = index.categoryId;
+
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(captionElement);
+
+    gallery.appendChild(figureElement);
+  });
+}
+
+fetch(apiUrl + "/works")
   .then((response) => response.json())
   .then((data) => {
-    function createImg(data) {
-      data.forEach((index) => {
-        const figureElement = document.createElement("figure");
-        const imgElement = document.createElement("img");
-        const captionElement = document.createElement("figcaption");
-
-        imgElement.src = index.imageUrl;
-        imgElement.alt = index.title;
-        captionElement.textContent = index.title;
-
-        figureElement.appendChild(imgElement);
-        figureElement.appendChild(captionElement);
-
-        gallery.appendChild(figureElement);
-      });
-    }
     createImg(data);
   });
